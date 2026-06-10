0a849b7f3340c7208bbd2b02d890129274c79777
ivorypaulina <ivorypaulina10@gmail.com>
2026-06-09 21:24:37 -0500

    Practica 1 - main

---
+++ src/main/java/com/example/tareafx2/MainApp.java
@@ -0,0 +1,26 @@
+package com.example.tareafx2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader =
+                new FXMLLoader(MainApp.class.getResource("Login.fxml"));
+
+        Scene scene = new Scene(loader.load());
+
+        stage.setTitle("Sistema Login");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
